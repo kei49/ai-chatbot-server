638902a62f7b2ea004dd2291501ef1c6a2bf23ab
Kei Shimizu <suns.manage.k@gmail.com>
2023-02-05 00:35:26 +0900

    removed psql db create

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,5 +1,11 @@
 const env = process.env.NODE_ENV || 'local';
-const availabeNodeEnv = ['local', 'development', 'producton', 'test'];
+const availabeNodeEnv = [
+  'local',
+  'development',
+  'producton',
+  'test',
+  'ci-test',
+];
 
 if (!availabeNodeEnv.includes(env)) {
   throw new Error(`Invalid NODE_ENV: please set the value`);
